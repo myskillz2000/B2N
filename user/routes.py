25e753726fb9d0f7ef13d12b47c74f69bb54a3b6
Matt Stiller <matthewstiller484@yahoo.com>
2022-06-03 10:01:06 -0400

    Modified for readability.

--- user/routes.py
+++ user/routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
 
 
-user_blueprint = Blueprint('user_api_routes', __name__, url_prefix='/api/user')
+user_blueprint = Blueprint('user_api_routes', __name__, 
+                            url_prefix='/api/user')
 
 @user_blueprint.route('/')
 def index():
