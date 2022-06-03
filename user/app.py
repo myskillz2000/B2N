db5a9aaa0d62f477df6c4d57f67cf4a0e8f51fc1
Matt Stiller <matthewstiller484@yahoo.com>
2022-06-03 12:22:28 -0400

    Connected the models to the application.

--- user/app.py
+++ user/app.py
@@ -1,7 +1,13 @@
+from statistics import mode
 from flask import Flask
+import models
 from routes import user_blueprint
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Matt'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./database/user.db'
+models.init_app(app)
+
 app.register_blueprint(user_blueprint)
 
 
